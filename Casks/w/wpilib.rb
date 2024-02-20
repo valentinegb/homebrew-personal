6fe9d62ca5bc85fb229e82bd9f16a0b98b4960b3
Valentine Briese <valentinegb@icloud.com>
2024-02-19 17:07:51 -0800

    Upgrade wpilib to v2024.3.1

--- Casks/w/wpilib.rb
+++ Casks/w/wpilib.rb
@@ -2,9 +2,9 @@ cask "wpilib" do
   arch arm: "Arm64", intel: "Intel"
   url_arch = on_arch_conditional arm: "Arm", intel: ""
 
-  version "2024.2.1"
-  sha256 arm:   "8ac5465408ba1d7ade6b17100093eb1d3717e7703b3bba381cb17e0df8b14c58",
-         intel: "ac2537bcafb91a78ed3488d113e8119d1b0c4da32af6d78866081422df7c41f3"
+  version "2024.3.1"
+  sha256 arm:   "14fd3891e3c4438d2a121af68ced5da0eaeab7fc4e12471312aaf21ae9d80cea",
+         intel: "0b41d45537585a4f913e2362c4a25ab32b220b2ba4a20ec009a8d8f0ce55606c"
 
   url "https://packages.wpilib.workers.dev/installer/v#{version}/macOS#{url_arch}/WPILib_macOS-#{arch}-#{version}.dmg",
       verified: "packages.wpilib.workers.dev/"
