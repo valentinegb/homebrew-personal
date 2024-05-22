5a6fa7752384e6956c34f2d4b608d1326482b8b5
Valentine Briese <valentinegb@icloud.com>
2024-05-21 17:32:39 -0700

    Improve `granite` dependency list

--- Formula/g/granite.rb
+++ Formula/g/granite.rb
@@ -5,26 +5,38 @@ class Granite < Formula
   sha256 "9439733169c07cfc658144ceef90bebac5f024ffda4bbb65e8c1ab68e5580908"
   license "LGPL-3.0-or-later"
 
-  depends_on "cairo" => :build
-  depends_on "gdk-pixbuf" => :build
-  depends_on "gettext" => :build
-  depends_on "glib" => :build
   depends_on "gobject-introspection" => :build
-  depends_on "graphene" => :build
-  depends_on "gtk4" => :build
-  depends_on "harfbuzz" => :build
-  depends_on "libgee" => :build
   depends_on "meson" => :build
   depends_on "ninja" => :build
-  depends_on "pango" => :build
   depends_on "pkg-config" => :build
   depends_on "sassc" => :build
   depends_on "vala" => :build
+  depends_on "cairo"
+  depends_on "fontconfig"
+  depends_on "freetype"
+  depends_on "fribidi"
+  depends_on "gdk-pixbuf"
+  depends_on "gettext"
+  depends_on "glib"
+  depends_on "graphene"
+  depends_on "graphite2"
+  depends_on "gsettings-desktop-schemas"
+  depends_on "gtk4"
+  depends_on "harfbuzz"
+  depends_on "jpeg-turbo"
+  depends_on "libgee"
+  depends_on "libpng"
+  depends_on "libx11"
+  depends_on "libxau"
+  depends_on "libxcb"
+  depends_on "libxdmcp"
+  depends_on "libxext"
+  depends_on "libxrender"
+  depends_on "pango"
+  depends_on "pixman"
+  depends_on "xorgproto"
 
   def install
-    ENV.delete "PKG_CONFIG_LIBDIR"
-    ENV.delete "PKG_CONFIG_PATH"
-    ENV.delete "PKG_CONFIG"
     system "meson", "setup", "build", *std_meson_args
     cd "build" do
       system "ninja"
