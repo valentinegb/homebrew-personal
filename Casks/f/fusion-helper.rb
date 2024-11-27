0479b54fcc85b909123450bb65e93088beaeb3f4
Valentine Briese <valentinegb@icloud.com>
2024-11-27 10:17:20 -0800

    fusion-helper 1.1.0

--- Casks/f/fusion-helper.rb
+++ Casks/f/fusion-helper.rb
@@ -1,16 +1,14 @@
 cask "fusion-helper" do
-  arch arm: "arm64", intel: "x64"
+  version "1.1.0"
+  sha256 "608af97e36ae27744e507ac1a490894b748e1153fea61a3e32be05d86116b89a"
 
-  version "1.0.0"
-  sha256 "1561bf79e894986a40c1c6fcd7ad98bfa99c5b85cc3b77c504fb50d735f52eea"
-
-  url "https://github.com/Lakatrazz/Fusion-Helper/releases/download/v#{version}/FusionHelper_mac_#{arch}.zip"
+  url "https://github.com/Lakatrazz/Fusion-Helper/releases/download/v#{version}/FusionHelper_Mac.zip"
   name "Fusion Helper"
-  desc "Proxy application to allow the use of BONELAB Fusion on the Meta Quest 2"
+  desc "Proxy application to allow the use of BONELAB Fusion on the Meta Quests"
   homepage "https://github.com/Lakatrazz/Fusion-Helper"
 
   depends_on macos: ">= :big_sur"
   depends_on cask: "steam"
 
-  app "FusionHelper.app"
+  app "FusionHelper_Mac.app"
 end
