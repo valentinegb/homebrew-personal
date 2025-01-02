d4e652f4cd6ff9273d5965699d3a46961d41db2e
Valentine Briese <valentinegb@icloud.com>
2025-01-02 11:21:40 -0800

    wpilib 2025.1.1

--- Casks/wpilib.rb
+++ Casks/wpilib.rb
@@ -2,9 +2,9 @@ cask "wpilib" do
   arch arm: "Arm64", intel: "Intel"
   url_arch = on_arch_conditional arm: "Arm", intel: ""
 
-  version "2024.3.2"
-  sha256 arm:   "1af409d3d05debb20120ab494f5d21f482ce270768f41221db2714c97e8db135",
-         intel: "ca551ee4e44ed1135b5caa0d70920ce29fd06f260099cb85da976081e66a7aac"
+  version "2025.1.1"
+  sha256 arm:   "e51bdecf830390b02a33e3203015aa53a198576e9dc0e23583a3ca08007d16eb",
+         intel: "340e69a32c4b6baa651a6a79f220fa6ef3ef637a6b25fa52c934adc5361ce47a"
 
   url "https://packages.wpilib.workers.dev/installer/v#{version}/macOS#{url_arch}/WPILib_macOS-#{arch}-#{version}.dmg",
       verified: "packages.wpilib.workers.dev/"
