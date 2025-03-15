45702ff703e441e1f3c4ca6ebe53a0b6b0cc4338
Valentine Briese <valentinegb@icloud.com>
2025-03-14 17:46:19 -0700

    radicle-tui: change "master" to "main"

--- Formula/radicle-tui.rb
+++ Formula/radicle-tui.rb
@@ -3,10 +3,10 @@ class RadicleTui < Formula
   homepage "https://app.radicle.xyz/nodes/seed.radicle.xyz/rad:z39mP9rQAaGmERfUMPULfPUi473tY"
   license any_of: ["MIT", "Apache-2.0"]
   head "https://seed.radicle.xyz/z39mP9rQAaGmERfUMPULfPUi473tY.git",
-       branch: "master"
+       branch: "main"
 
   depends_on "rust" => :build
-    depends_on "openssh"
+  depends_on "openssh"
 
   def install
     system "cargo", "install", *std_cargo_args
